b79545d748698060d375d1bd069fe227fd2146a7
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2025-11-30 18:39:41 -0400

    feat: Refactor registerController to include verification token generation and improve response structure

--- apps/server/src/controllers/auth/register.controller.ts
+++ apps/server/src/controllers/auth/register.controller.ts
@@ -1,7 +1,9 @@
 import type { Request, Response } from 'express';
 import { UserZodSchema } from '../../types/user.types.js';
 import UserModel from '../../models/user.model.js';
-import hashPassword from '../../lib/hashPassword.js';
+import hashPassword from '../../utils/hashPassword.js';
+import getUserWithoutPass from '../../utils/getUserWithoutPass.js';
+// import sendEmail from '../../service/sendEmail.service.js';
 
 export async function registerController(req: Request, res: Response) {
     try {
@@ -9,24 +11,25 @@ export async function registerController(req: Request, res: Response) {
         const isUserExist = await UserModel.findOne({ email });
         if (isUserExist) throw new Error('User already exists');
 
+        const token = Math.floor(100000 + Math.random() * 900000).toString();
         const newUser = new UserModel({
             username,
             email,
             password: hashPassword(password),
         });
 
+        newUser.verificationToken = token;
+        newUser.verificationTokenExpire = new Date(Date.now() + 10 * 60 * 1000);
+
+        // ? send email
+        console.log(token);
+        // await sendEmail({ code: token, to: email, text: 'Text', subject: 'text' });
+
         const savedUser = await newUser.save();
+        const userWithoutPassword = getUserWithoutPass(savedUser);
 
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const { password: _, ...userWithoutPassword } = savedUser.toObject();
-
-        return res.json({
-            success: true,
-            message: 'User registered successfully',
-            data: userWithoutPassword,
-        });
+        return res.json({ ok: true, message: 'User registered successfully', data: userWithoutPassword });
     } catch (err) {
-        console.log('a')
-        return res.status(400).json({ success: false, message: (err as Error).message });
+        return res.status(400).json({ ok: false, message: (err as Error).message });
     }
 }
